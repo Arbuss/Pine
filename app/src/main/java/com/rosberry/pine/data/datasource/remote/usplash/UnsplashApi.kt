e0301ad2767b4c16972385f0405cb782240747a3
Artur Buss <artur.buss@rosberry.com>
2021-09-01 12:35:26 +0600

    Added interceptor

--- app/src/main/java/com/rosberry/pine/data/datasource/remote/usplash/UnsplashApi.kt
+++ app/src/main/java/com/rosberry/pine/data/datasource/remote/usplash/UnsplashApi.kt
@@ -1,15 +1,12 @@
 package com.rosberry.pine.data.datasource.remote.usplash
 
-import com.rosberry.pine.BuildConfig
 import com.rosberry.pine.data.repository.model.Image
 import retrofit2.Call
 import retrofit2.http.GET
-import retrofit2.http.Headers
 import retrofit2.http.Query
 
 interface UnsplashApi {
 
-    @Headers("Authorization: Client-ID ${BuildConfig.API_KEY}")
     @GET("/photos")
     fun getPage(
             @Query("page") page: Int,
--- app/src/main/java/com/rosberry/pine/di/NetworkingModule.kt
+++ app/src/main/java/com/rosberry/pine/di/NetworkingModule.kt
@@ -6,6 +6,8 @@ import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import okhttp3.Interceptor
+import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import javax.inject.Singleton
@@ -16,8 +18,27 @@ object NetworkingModule {
 
     @Provides
     @Singleton
-    fun provideRetrofit(): Retrofit = Retrofit.Builder()
+    fun provideOkHttpClient(): OkHttpClient {
+        val okHttpBuilder = OkHttpClient.Builder()
+        val interceptor = Interceptor { chain ->
+            chain.run {
+                proceed(
+                        request()
+                            .newBuilder()
+                            .addHeader("Authorization", "Client-ID ${BuildConfig.API_KEY}")
+                            .build()
+                )
+            }
+        }
+        okHttpBuilder.addInterceptor(interceptor)
+        return okHttpBuilder.build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideRetrofit(okHttpClient: OkHttpClient): Retrofit = Retrofit.Builder()
         .baseUrl(BuildConfig.BASE_URL)
+        .client(okHttpClient)
         .addConverterFactory(GsonConverterFactory.create())
         .build()
 
