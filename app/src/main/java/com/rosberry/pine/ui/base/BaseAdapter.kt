bf2aa3a743abeade23ca32789a87d6c4631f7ba2
Artur Buss <artur.buss@rosberry.com>
2021-09-02 12:32:59 +0600

    Added Diff Callback

--- app/src/main/java/com/rosberry/pine/ui/base/BaseAdapter.kt
+++ app/src/main/java/com/rosberry/pine/ui/base/BaseAdapter.kt
@@ -1,6 +1,7 @@
 package com.rosberry.pine.ui.base
 
 import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
 import androidx.viewbinding.ViewBinding
 
@@ -17,14 +18,26 @@ abstract class BaseAdapter<T : BaseAdapterItem,
     override fun getItemCount() = items.size
 
     fun addItems(newItems: List<T>) {
-        val lastIndex = items.lastIndex
+        val diffUtilCallback = createDiffUtilCallback(newItems)
+        val diffResult = DiffUtil.calculateDiff(diffUtilCallback)
+        items.clear()
         items.addAll(newItems)
-        notifyItemRangeInserted(lastIndex, lastIndex + newItems.size)
+        diffResult.dispatchUpdatesTo(this)
     }
+
+    abstract fun createDiffUtilCallback(newList: List<T>): BaseDiffUtilCallback<T>
 
     abstract class BaseViewHolder<in T : BaseAdapterItem, B : ViewBinding>(val binding: B) :
             RecyclerView.ViewHolder(binding.root) {
 
         abstract fun bind(item: T)
+    }
+
+    abstract inner class BaseDiffUtilCallback<T>(protected val newList: List<T>) :
+            DiffUtil.Callback() {
+
+        override fun getOldListSize() = items.size
+
+        override fun getNewListSize() = newList.size
     }
 }
