5dc27081ca738026e4ff5b27e371df36f16e84e6
Artur Buss <artur.buss@rosberry.com>
2021-09-09 17:52:28 +0600

    Deleted incorrect binding code

--- app/src/main/java/com/rosberry/pine/ui/feed/FeedFragment.kt
+++ app/src/main/java/com/rosberry/pine/ui/feed/FeedFragment.kt
@@ -4,7 +4,6 @@ import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.fragment.app.viewModels
 import com.rosberry.pine.databinding.FragmentFeedBinding
-import com.rosberry.pine.databinding.ViewFeedBinding
 import com.rosberry.pine.ui.base.ListedFragment
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -12,12 +11,8 @@ import dagger.hilt.android.AndroidEntryPoint
 class FeedFragment : ListedFragment<FragmentFeedBinding>() {
 
     override val viewModel: FeedViewModel by viewModels()
+    
+    override fun createViewBinding(inflater: LayoutInflater, container: ViewGroup?): FragmentFeedBinding? =
+            FragmentFeedBinding.inflate(inflater, container, false)
 
-    var feedViewBinding: ViewFeedBinding? = null
-
-    override fun createViewBinding(inflater: LayoutInflater, container: ViewGroup?): FragmentFeedBinding? {
-        val bnd = FragmentFeedBinding.inflate(inflater, container, false)
-        feedViewBinding = ViewFeedBinding.bind(bnd.root)
-        return bnd
-    }
 }
